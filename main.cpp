09b9e07622af4b3fef78658c219da0ea780123cb
Coty Strumia <127853938+CotyStrumia@users.noreply.github.com>
2023-10-22 19:41:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <sstream>
 #include <limits>
+#include <cstring>
 using namespace std;
 
 DatosProductos base_de_datos[400];
@@ -47,7 +48,13 @@ void leerArchivo() {
 
             switch (cont) {
                 case 0: {
-                    base_de_datos[ubicProducto].setgrupo(dato);
+                    if(dato.empty()){
+                        dato=base_de_datos[ubicProducto-1].getgrupo();
+                    }
+                        base_de_datos[ubicProducto].setgrupo(dato);
+
+
+
                     break;
                 }
                 case 1: {
@@ -110,10 +117,44 @@ void crearArbol(){
 }
 
 void minStock(int n){
-
+    for(int i=0; i<ubicProducto; i++) {
+        if (base_de_datos[i].getstockTotal() <= n) {
+            base_de_datos[i].probador();
+            cout<<"-----------------------------------------------------"<<endl;
+        }
+    }
 }
 
+void maxStock(int n){
+    for(int i=0; i<ubicProducto; i++) {
+        if (base_de_datos[i].getstockTotal() >= n) {
+            base_de_datos[i].probador();
+            cout<<"-----------------------------------------------------"<<endl;
+        }
+    }
+}
 
+void stock() {
+    char argumento[60];
+    char p;
+    int i = 0;
+
+    cout << "Ingrese el producto a buscar, finalice con un punto: ";
+    cin >> ws; // Elimina espacios en blanco iniciales si los hubiera.
+    while (i < 59 && (cin.get(p) && p != '.' && p != '\n')) {
+        argumento[i] = p;
+        i++;
+    }
+    argumento[i] = '\0'; // Agrega el carácter nulo al final de la cadena.
+
+    for (int j = 0; j < ubicProducto; j++) {
+        if (strcmp(base_de_datos[j].getnombre(), argumento) == 0) {
+            cout << "-----------------------------------------------------" << endl;
+            cout << "STOCK TOTAL DEL PRODUCTO: " << base_de_datos[j].getstockTotal() << endl;
+            cout << "-----------------------------------------------------" << endl;
+        }
+    }
+}
 
 
 int main() {
@@ -121,102 +162,99 @@ int main() {
     int opcswitch, n, deposito;
     string opc;
 
+
     leerArchivo();
 
     crearArbol();
 
-
-
-
     while(opc!="salir") {
-         cout << "INGRESE POR TECLADO LO QUE QUIERE REALIZAR" << endl;
-         cout << "'total_art_dif' Cantidad total de articulos diferentes." << endl
-              << "'total_art' Cantidad total de articulos" << endl
-              << "'min_stock' Listado de articulos con cantidad n o menos de stock." << endl
-              << "'min_stock_deposito' Listado de articulos con cantidad n o menos de stock segun un deposito" << endl
-              << "'stock' El stock total del articulo ingresado como argumento." << endl
-              << "'stock_deposito' El stock del articulo en un deposito." << endl
-              << "'max_stock' Listado de aquellos articulos cuyo stock es igual o supera el numero n." << endl
-              << "'salir' Salir del programa" << endl;
-         cout << "OPCION: ";
-         cin >> opc;
+        cout << "INGRESE POR TECLADO LO QUE QUIERE REALIZAR" << endl;
+        cout << "'total_art_dif' Cantidad total de articulos diferentes." << endl
+             << "'total_art' Cantidad total de articulos" << endl
+             << "'min_stock' Listado de articulos con cantidad n o menos de stock." << endl
+             << "'min_stock_deposito' Listado de articulos con cantidad n o menos de stock segun un deposito" << endl
+             << "'stock' El stock total del articulo ingresado como argumento." << endl
+             << "'stock_deposito' El stock del articulo en un deposito." << endl
+             << "'max_stock' Listado de aquellos articulos cuyo stock es igual o supera el numero n." << endl
+             << "'salir' Salir del programa" << endl;
+        cout << "OPCION: ";
+        cin >> opc;
 
-         while (opc != "total_art_dif" && opc != "total_art" && opc != "min_stock" && opc != "min_stock_deposito" &&
-                opc != "stock" && opc != "stock_deposito" && opc != "max_stock" && opc != "salir") {
-             cout << "La opcion ingresada no es valida, reingrese la opcion: ";
-             cin >> opc;
-         }
+        while (opc != "total_art_dif" && opc != "total_art" && opc != "min_stock" && opc != "min_stock_deposito" &&
+               opc != "stock" && opc != "stock_deposito" && opc != "max_stock" && opc != "salir") {
+            cout << "La opcion ingresada no es valida, reingrese la opcion: ";
+            cin >> opc;
+        }
 
-         if (opc == "total_art_dif") {
-             opcswitch = 1;
-         }
-         if (opc == "total_art") {
-             opcswitch = 2;
-         }
-         if (opc == "min_stock") {
-             opcswitch = 3;
-         }
-         if (opc == "min_stock_deposito") {
-             opcswitch = 4;
-         }
-         if (opc == "stock") {
-             opcswitch = 5;
-         }
-         if (opc == "stock_deposito") {
-             opcswitch = 6;
-         }
-         if (opc == "max_stock") {
-             opcswitch = 7;
-         }
+        if (opc == "total_art_dif") {
+            opcswitch = 1;
+        }
+        if (opc == "total_art") {
+            opcswitch = 2;
+        }
+        if (opc == "min_stock") {
+            opcswitch = 3;
+        }
+        if (opc == "min_stock_deposito") {
+            opcswitch = 4;
+        }
+        if (opc == "stock") {
+            opcswitch = 5;
+        }
+        if (opc == "stock_deposito") {
+            opcswitch = 6;
+        }
+        if (opc == "max_stock") {
+            opcswitch = 7;
+        }
 
-         switch (opcswitch) {
-             case 1:
-                 cout<<"El total de articulos diferentes es: ";
-                 cout<<ubicProducto<<endl;
-                 cout<<endl<<""<<endl;
-                 break;
-             case 2:
-                 cout<< "La cantidad total de articulos es: ";
-                 cout<<stockGeneral<<endl;
-                 cout<<endl<<""<<endl;
-                 break;
-             case 3:
-                 cout<< "ingrese la cantidad de stock a analizar menor o igual: ";
-                 cin >> n;
-                 cout<<"El listado de articulos con cantidad menor o igual a "<< n << " de stock es: ";
+        switch (opcswitch) {
+            case 1:
+                cout<<"El total de articulos diferentes es: ";
+                cout<<ubicProducto<<endl;
+                cout<<endl<<""<<endl;
+                break;
+            case 2:
+                cout<< "La cantidad total de articulos es: ";
+                cout<<stockGeneral<<endl;
+                cout<<endl<<""<<endl;
+                break;
+            case 3:
+                cout<< "ingrese la cantidad de stock a analizar menor o igual: ";
+                cin >> n;
+                cout<<"El listado de articulos con cantidad menor o igual a "<< n << " de stock es: "<<endl;
+                cout<<endl;
+                minStock(n);
+                cout<<endl<<""<<endl;
+                break;
+            case 4:
+                cout<< "ingrese la cantidad minima de stock: ";
+                cin >> n;
+                cout<< "ingrese el deposito que quiere ver: ";
+                cin >> deposito;
+                cout<<"El listado de articulos en el deposito "<< deposito<< " con cantidad menor o igual a "<< n << " de stock es: ";
 
-                 cout<<endl<<""<<endl;
-                 break;
-             case 4:
-                 cout<< "ingrese la cantidad minima de stock: ";
-                 cin >> n;
-                 cout<< "ingrese el deposito que quiere ver: ";
-                 cin >> deposito;
-                 cout<<"El listado de articulos en el deposito "<< deposito<< " con cantidad menor o igual a "<< n << " de stock es: ";
+                cout<<endl<<""<<endl;
+                break;
+            case 5:
+                stock();
+                cout<<endl<<""<<endl;
+                break;
+            case 6:
 
-                 cout<<endl<<""<<endl;
-                 break;
-             case 5:
+                cout<< "ingrese el deposito que quiere ver: ";
+                cin >> deposito;
+                cout << "el stock del articulo en el deposito "<< deposito << " es: ";
 
-                 cout << "el stock del articulo es: ";
-
-                 cout<<endl<<""<<endl;
-                 break;
-             case 6:
-
-                 cout<< "ingrese el deposito que quiere ver: ";
-                 cin >> deposito;
-                 cout << "el stock del articulo en el deposito "<< deposito << " es: ";
-
-                 cout<<endl<<""<<endl;
-                 break;
-             case 7:
-                 cout<< "ingrese la cantidad de stock a analizar mayor o igual: ";
-                 cin >> n;
-                 cout<<"El listado de articulos con cantidad mayor o igual a "<< n << " de stock es: ";
-
-                 cout<<endl<<""<<endl;
-                 break;
-         }
-     }
+                cout<<endl<<""<<endl;
+                break;
+            case 7:
+                cout<< "ingrese la cantidad de stock a analizar mayor o igual: ";
+                cin >> n;
+                cout<<"El listado de articulos con cantidad mayor o igual a "<< n << " de stock es: "<<endl;
+                maxStock(n);
+                cout<<endl<<""<<endl;
+                break;
+        }
+    }
 }
